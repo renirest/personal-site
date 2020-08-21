35f8ffac05ed6641d03d8d2ff02ae4952bcde2bf
renirest <renirest@gmail.com>
2020-08-21 00:40:24 -0400

    change project

--- app/data/projects.js
+++ app/data/projects.js
@@ -28,15 +28,6 @@ const data = [
     desc: 'Launched a potato battery powered weather balloon with two cameras '
       + 'and gps transponder. Resulting photos were published in a coffee table book. '
       + 'You can email me for a copy.',
-  }, {
-    title: 'Cat Detector',
-    subtitle: 'A convolutional neural network to classify cats! (and dogs)',
-    link: '/project/catdetector/',
-    image: '/images/projects/catdetector.jpg',
-    date: '2015-05-15',
-    desc: 'Trained a convolutional neural network to classify between ~ 80 cats breeds. '
-     + 'Over 60,000 cats were classified before server bills made the project too expensive '
-     + 'to continue hosting.',
   },
 ];
 
